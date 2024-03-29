00f61cda57284b229648910cc02429b1667d3cfb
AdityaTohan <61872854+AdityaTohan@users.noreply.github.com>
2024-03-29 06:30:21 +0000

    malnutrition

--- pages/Malnutrition Analysis.py
+++ pages/Malnutrition Analysis.py
@@ -23,13 +23,13 @@ def main():
     st.sidebar.title("Filter Options")
     selected_countries = st.sidebar.multiselect("Select Countries", df['Country Name'].unique())
     selected_gender = st.sidebar.selectbox("Select Gender", df['Sex'].unique())
-    selected_y_axis = st.sidebar.selectbox("Select Y-axis", df.columns, index=5)  # Default to 'Mean' for y-axis
+    selected_y_axis = st.sidebar.selectbox("Select Y-axis", ['Overweight', 'Stunting', 'Wasting', 'Mean'], index=3)  # Default to 'Mean' for y-axis
 
     # Filter data based on user selection
     filtered_df = df[(df['Country Name'].isin(selected_countries)) & (df['Sex'] == selected_gender)]
 
     # Plot dynamic graph
-    st.header("Dynamic Analysis")
+    st.header("Dynamic Analysis Graph")
     if not filtered_df.empty:
         fig = go.Figure()
 
@@ -49,31 +49,51 @@ def main():
         st.plotly_chart(fig)
     else:
         st.warning(f"No data available for the selected {selected_gender}s in the selected countries.")
-
+    
+    st.empty()
     # Plot static bar graphs
-    st.header("Global Analysis")
+    st.header("Global Malnutrition Analysis")
     st.subheader("Histograms of Malnutrition Indicators")
 
     with plt.style.context("dark_background"):  # Use dark background style for static graphs
         fig, axis = plt.subplots(2,2)
         fig.suptitle("World's Children Malnutrition Histogram", fontsize=20)
 
-        axis[0,0].hist(df['Mean'], bins=30, color='white', alpha=0.7, edgecolor='black')
+        axis[0,0].hist(df['Mean'], bins=50, color='lightblue', alpha=0.7, edgecolor='black')
         axis[0,0].set_xlabel('Mean Malnutrition Percentage', fontsize=14)
         axis[0,0].set_ylabel('Number of Countries')
 
-        axis[0,1].hist(df['Overweight'], bins=30, color='lightgreen', alpha=0.7, edgecolor='black')
+        axis[0,1].hist(df['Overweight'], bins=50, color='lightgreen', alpha=0.7, edgecolor='black')
         axis[0,1].set_xlabel('Overweight Percentage', fontsize=14)
         axis[0,1].set_ylabel('Number of Countries')
 
-        axis[1,0].hist(df['Wasting'], bins=30, color='lightcoral', alpha=0.7, edgecolor='black')
+        axis[1,0].hist(df['Wasting'], bins=50, color='lightcoral', alpha=0.7, edgecolor='black')
         axis[1,0].set_xlabel('Wasting Percentage', fontsize=14)
         axis[1,0].set_ylabel('Number of Countries')
 
-        axis[1,1].hist(df['Stunting'], bins=30, color='lightskyblue', alpha=0.7, edgecolor='black')
+        axis[1,1].hist(df['Stunting'], bins=50, color='lightskyblue', alpha=0.7, edgecolor='black')
         axis[1,1].set_xlabel('Stunting Percentage', fontsize=14)
         axis[1,1].set_ylabel('Number of Countries')
 
+        fig.set_size_inches(15, 10)
+        st.pyplot(fig)
+
+    # Plot the fifth static graph in a separate component
+    st.empty()
+    st.empty()
+    
+    st.subheader("Combined Global Malnutrition Analysis Histogram")
+
+    with plt.style.context("dark_background"):  # Use darkroyalblue style for static graphs
+        fig, ax = plt.subplots()
+        ax.hist(df['Overweight'], bins=30, color='lightblue', alpha=0.5, label='Overweight', edgecolor='black')
+        ax.hist(df['Stunting'], bins=30, color='royalblue', alpha=0.5, label='Stunting', edgecolor='black')
+        ax.hist(df['Wasting'], bins=30, color='orange', alpha=0.5, label='Wasting', edgecolor='black')
+        ax.hist(df['Mean'], bins=30, color='forestgreen', alpha=0.5, label='Mean', edgecolor='black')
+        ax.set_xlabel('Percentage', fontsize=14)
+        ax.set_ylabel('Number of Countries', fontsize=16)
+
+        ax.legend()
         fig.set_size_inches(15, 10)
         st.pyplot(fig)
 
